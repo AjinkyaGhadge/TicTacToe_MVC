3112b90b32492fead387591be39ac981bbe5a2de
pmhatre1 <pmhatre1@asu.edu>
2024-08-31 17:44:48 -0600

    Adding RedLightTheme

---
+++ TicTacToe_2/ThemeLogic/ThemeFactory/RedLightThemeFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using View.ThemeLogic.Theme;
+using View;
+
+namespace View.ThemeLogic.ThemeFactory
+{
+    public class RedLightThemeFactory:AbstractThemeFactory
+    {
+        public override AbstractTheme CreateTheme() => new RedLightTheme();
+    }
+}
